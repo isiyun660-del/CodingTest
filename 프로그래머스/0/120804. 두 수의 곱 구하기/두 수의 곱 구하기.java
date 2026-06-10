1d2036bcaff46b65ac2b3b752af2dfded9d81114
isiyun660-del <isiyun660@gmail.com>
2026-06-10 12:44:34 +0900

    전체 코드 업로드 -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int solution(int unm1, int unm2) {
+        int answer = unm1 * unm2;
+        return answer;
+    }
+}
---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    return answer = (n +6) / 7;
+    return answer;
+}
